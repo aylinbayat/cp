54f41b93dd5ab552123cdc9e44a64cab214f9f45
aylinbayat <117563715+aylinbayat@users.noreply.github.com>
2022-11-06 23:33:48 +0330

    Add files via upload

---
+++ main4-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main( ) {
+	int x , y , z;
+	int result0 , result1;
+	cout << " please enter x : ";
+	cin >> x;
+	cout << " please enter y : ";
+	cin >> y;
+	cout << " please enter z : ";
+	cin >> z;
+	result0 = (x-y)*(x+z);
+	result1 = (x+y+z, 2) + 1;
+	cout << result0 / result1;
+}
